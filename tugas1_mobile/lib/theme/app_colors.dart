624f4ee6eba23ca7bb754bb369a94f77d09d8555
Cintasir18 <124240011@student.upnyk.ac.id>
2026-09-04 14:35:47 +0700

    menambah design sistem

--- tugas1_mobile/lib/theme/app_colors.dart
+++ tugas1_mobile/lib/theme/app_colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const primaryColor = Color(0xFF242D4C);
-  static const secondaryColor = Color(0xFFF0EAD6);
-} I Yeah, she hung up.
+  static const primaryColor = Color(0xFF013E37);
+  static const secondaryColor = Color(0xFFFFEFB4);
+}
